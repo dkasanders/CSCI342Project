9dfd3b5cb205ae4eb434aa57db036e533c20af85
Ian Crawford <crawfoi2@wwu.edu>
2024-02-13 14:59:04 -0800

    added api.js file to be used for api calls

---
+++ CSCI342PROJECT/src/Api.js
@@ -0,0 +1,7 @@
+// import axios from 'axios';
+// import resolve from './resolve.js'
+
+//functions below like 
+//export async function thisCallsAnApi () {
+// (here we can interact with our mongodb if we need to and return data)
+//}
