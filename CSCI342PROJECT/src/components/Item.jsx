f15a0ea3d12f08b8accf8d47a5a79029c1921912
RyanRap <55864660+RyanRap@users.noreply.github.com>
2024-02-26 17:50:11 -0800

    Added Item page
    page that will be used when an item is selected, will contain the item image and description

---
+++ CSCI342PROJECT/src/components/Item.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../Header/Header";
+function Item() {
+    return (
+        <>
+        <Header/>
+        <p>Item Page</p>
+        </>
+    )
+}
+
+export default Item;
