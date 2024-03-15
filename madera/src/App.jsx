3f37272e7bb1c5a9c39a4caad2379e7904ee2a2f
dkasanders <dkasanders00@gmail.com>
2024-03-14 17:46:36 -0700

    product

--- madera/src/App.jsx
+++ madera/src/App.jsx
@@ -7,17 +7,36 @@ import Header from './components/Header/Header'
 import About from './components/About/About'
 import Categories from './components/Categories/Categories'
 import {Link, Outlet} from 'react-router-dom'
+
+import backgroundImage from './assets/background_image.png';
+import Product from './components/Product/Product'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
+    /*
   return (
-    <>
+    <div className="app">
       <Header />
       <Categories />
       <About/>
       <Footer/>
-    </>
+    </div>
   )
+  */
+
+  const dummyData = {
+    'name': "Wood Chair",
+    'description': "Really good wooden chair.",
+    'category': "wood",
+    'images' : [{data: 'hmm', 'type': 'hmmm'}],
+    'price': 100,
+    'inventory': 3
+  };
+ return (
+  <Product productData={dummyData}/>
+ )
 }
 
 export default App
--- madera/src/components/Header/Header.jsx
+++ madera/src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import logo from '../../assets/logo.png';
+
+import './Header.css'
 
 function Header() {
 
     return (
-        <div>
-            <h1>Title</h1>
-            <Link to="/shop">Shop</Link>
-            <Link to="/about">About Us</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/login">Login</Link>
-
+        <div className={`flex justify-between items-center py-4 px-8`}>
+            <div className={`flex items-center`}>
+                <img className={`h-8  mr-2`} alt="Logo" src={logo} />
+                <Link to="/" className={`text-lg font-semibold text-white text-black outline-black hover:text-gray-600`}><h1>MADERIA</h1></Link>
+            </div>
+            <div className="">
+                <div className={`flex space-x-4`}>
+                    <Link to="/shop" className={`text-gray-800 hover:text-gray-600`}>Shop</Link>
+                    <Link to="/about" className={`text-gray-800 hover:text-gray-600`}>About Us</Link>
+                    <Link to="/cart" className={`text-gray-800 hover:text-gray-600`}>Cart</Link>
+                </div>
+            </div>    
+            <div>
+                <Link to="/login" className={`text-gray-800 hover:text-gray-600`}>Login</Link>
+            </div>
         </div>
     )
 }
