26ac2be13d57e892635d3ef7b78d5748457ce0e6
sochaa404 <sochaa@wwu.edu>
2024-03-14 17:43:17 -0700

    Added cart and address api

--- server/models.js
+++ server/models.js
@@ -77,7 +77,8 @@ const cartSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        require: true
+        require: true,
+        unique: true
     },
     products: [{
             product: {
--- server/server.js
+++ server/server.js
@@ -215,6 +215,28 @@ app.post("/login", async (req, res) => {
         response.status(500).send({ message: error.message });
       }
     })
+    .post(async (req, res) => {
+      // validate all of the necessary fields
+      // return created response with the new object or an error
+      const {name, description, categories, images, price, inventory} = req.body;
+      if (!name || !description || !categories || !price || !inventory ) {
+        return res.status(400).json({ message: "All fields are required." });
+      }
+      try {
+          const product = new Models.Product({
+            name, 
+            description,
+            categories,   
+            images,  
+            price,
+            inventory,
+          });
+          const data = product.save();
+          res.status(201).json({message: "Product created successfully", data});
+      } catch (err) {
+        return res.status(500).json({ message: err.message || "Error while creating product" });
+      }
+    })
     .put(async (req, res) => {
       // find one product and update
       // return created response with the updated object or an error
@@ -237,32 +259,165 @@ app.post("/login", async (req, res) => {
         }
         )
     })
+
+
+//--------------------ADDRESS API------------------
+app.route("/address")
+    .get(async (req, res) => {
+      if (!req.body) {
+        return res.status(404).send({
+          message: "No ID found"
+        });
+      }
+      const {_id} = req.body
+      try {
+        const address = await Models.Address.findById(_id);
+        res.send(address);
+      }
+      catch (error){
+        response.status(500).send({ message: error.message });
+      }
+    })
     .post(async (req, res) => {
-      // validate all of the necessary fields
-      // return created response with the new object or an error
-      const {name, categories, images, price, inventory} = req.body;
-      if (!name || !categories || !price || !inventory ) {
+      const {user, streetAddress, zipCode, state, primary, billing} = req.body;
+      if (!user || !streetAddress || !zipCode || !state || !primary || !billing) {
         return res.status(400).json({ message: "All fields are required." });
       }
       try {
-          const product = new Models.Product({
-            name, 
-            categories,   
-            images,  
-            price,
-            inventory,
+          const address = new Models.Product({
+            user, 
+            streetAddress,   
+            zipCode,  
+            state,
+            primary,
+            billing
           });
-          const data = product.save();
-          res.status(201).json({message: "Product created successfully", data});
+          const data = address.save();
+          res.status(201).json({message: "Address created successfully", data});
       } catch (err) {
         return res.status(500).json({ message: err.message || "Error while creating product" });
       }
+    })
+    .put(async (req, res) => {
+      if (!req.body) {
+        return res.status(400).send({
+          message: "Data to update can not be empty!"
+        });
+      }
+      const {_id} = req.body
+      Models.Address.findByIdAndUpdate(_id, req.body)
+        .then(data => {
+          if (!data) {
+            res.status(404).send({
+              message: `Cannot update Address with id=${id}.`
+            });
+          }
+          else {
+            res.status(201).json({message: "Address updated successfully", data})
+          }
+        }
+      )
     })
 
 
 
 
+//------------------Cart API------------------
 
+app.route("/cart")
+    .get(async (req, res) => {
+      if (!req.body) {
+        return res.status(404).send({
+          message: "No ID found"
+        });
+      }
+      const {user} = req.body
+      try {
+        const cart = await Models.Cart.findOne({user: user});
+        res.send(cart);
+      }
+      catch (error){
+        response.status(500).send({ message: error.message });
+      }
+    })
+    .post(async (req, res) => {
+      const {user} = req.body;
+      if (!user) {
+        return res.status(400).json({ message: "user Id required" });
+      }
+      try {
+        const user = await Models.User.findOne({ _id: user});
+        if (!user) {
+          res.status(404).send({
+            message: "No User found"
+          });
+        }
+      }
+      catch (error) {
+        response.status(500).send({ message: error.message });
+      }
+      
+      try {
+          const cart = new Models.Product({
+            user, 
+            products: []
+          });
+          const data = cart.save();
+          res.status(201).json({message: "Cart created successfully", data});
+      } catch (err) {
+        return res.status(500).json({ message: err.message || "Error while creating product" });
+      }
+    })
+    .put(async (req, res) => {
+      if (!req.body) {
+        return res.status(400).send({
+          message: "Data to update can not be empty!"
+        });
+      }
+      const {_id} = req.body
+      Models.Cart.findByIdAndUpdate(_id,  req.body)
+        .then(data => {
+          if (!data) {
+            res.status(404).send({
+              message: `Cannot update Cart with id=${id}.`
+            });
+          }
+          else {
+            res.status(201).json({message: "Cart updated successfully", data})
+          }
+        }
+      )
+    });
+
+    app.put("/cart/newproduct", async (req, res) => {
+      const {user, product, quantity} = res.body;
+      if (!user|| !product || !quantity) {
+        return res.status(400).json({ message: "user ID, product ID, and quantity required" });
+      }
+      try {
+        const usercheck = await Models.User.findOne({ _id: user});
+        if (!usercheck) {
+          res.status(404).send({
+            message: "No User found"
+          });
+        }
+        const productcheck = await Models.Product.findOne({ _id: product});
+        if (!productcheck) {
+          res.status(404).send({
+            message: "No product found"
+          });
+        }
+        const cart = await Models.Cart.findOne({user: user});
+
+        const newproduct = {product: product, quantity: quantity};
+        cart.products.push(newproduct);
+        cart.save();
+        res.status(201).send({message: "Cart updated successfully"})
+      }
+      catch (error) {
+        response.status(500).send({ message: error.message });
+      }
+    });
 
   // ---------------------PATHS-------------------------------
   app.get("/", (req, res) => {
