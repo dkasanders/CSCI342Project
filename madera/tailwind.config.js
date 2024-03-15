f05cbff71a9bb7f91e4184b120bf1a54d76a933b
Ian Crawford <crawfoi2@wwu.edu>
2024-03-15 09:30:05 -0700

    fixed active merge conflicts

--- madera/tailwind.config.js
+++ madera/tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-<<<<<<< HEAD
-    "./src/*.jsx",
-    "./src/components/**/*.jsx"
-=======
     './src/*.jsx',
     './src/components/**/*.jsx'
->>>>>>> group
   ],
   theme: {
     extend: {},
