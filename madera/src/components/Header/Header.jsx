db03e06a4d2e9b9f8492304719bcbad2b139a02a
RyanRap <55864660+RyanRap@users.noreply.github.com>
2024-03-14 21:00:46 -0700

    fixing

--- madera/src/components/Header/Header.jsx
+++ madera/src/components/Header/Header.jsx
@@ -1,35 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 
 import logo from '../../assets/logo.png';
 
 import './Header.css'
-=======
+
 import SearchBar from '../searchBar/SearchBar'
 import './../../index.css'
->>>>>>> group
 
 function Header() {
 
     return (
-<<<<<<< HEAD
-        <div className={`flex justify-between items-center py-4 px-8`}>
-            <div className={`flex items-center`}>
-                <img className={`h-8  mr-2`} alt="Logo" src={logo} />
-                <Link to="/" className={`text-lg font-semibold text-white text-black outline-black hover:text-gray-600`}><h1>MADERIA</h1></Link>
-            </div>
-            <div className="">
-                <div className={`flex space-x-4`}>
-                    <Link to="/shop" className={`text-gray-800 hover:text-gray-600`}>Shop</Link>
-                    <Link to="/about" className={`text-gray-800 hover:text-gray-600`}>About Us</Link>
-                    <Link to="/cart" className={`text-gray-800 hover:text-gray-600`}>Cart</Link>
-                </div>
-            </div>    
-            <div>
-                <Link to="/login" className={`text-gray-800 hover:text-gray-600`}>Login</Link>
-            </div>
-=======
         <div>
             <h1><Link to="/">Madera</Link></h1>
             <SearchBar/>
@@ -38,7 +19,6 @@ function Header() {
             <Link to="/cart">Cart</Link>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
->>>>>>> group
         </div>
     )
 }
